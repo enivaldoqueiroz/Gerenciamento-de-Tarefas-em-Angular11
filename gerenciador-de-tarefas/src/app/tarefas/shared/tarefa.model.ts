7f9503a2e0b8aed614a1a3dd32994ed2c7a123a2
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-02-08 22:07:42 -0300

    Criando o model de tarefas

---
+++ gerenciador-de-tarefas/src/app/tarefas/shared/tarefa.model.ts
@@ -0,0 +1,8 @@
+export class Terefa {
+    
+    constructor(
+        //?: - utilizando o operador ? os valores são opcional 
+        public id?: number,
+        public nome?: string,
+        public concluida?: boolean){}
+}
