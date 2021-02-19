15adcc970db0f905959ebc10c130a3afa11099a8
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-02-19 18:00:47 -0300

    Registrando as rotas de edição de tarefas

--- gerenciador-de-tarefas/src/app/app.module.ts
+++ gerenciador-de-tarefas/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { TarefasModule } from './tarefas';
 
+
 @NgModule({
   declarations: [
     AppComponent
